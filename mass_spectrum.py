bf8b710c25f6632d12b2ebe6406bfe019b4e2358
palmer <andrew.palmer@embl.de>
2015-03-20 15:17:32 +0100

    changed methods for getting data from object: now both profile and/or centroids can be stored in the same instance get_spectrum method return both with argument of 'profile' or 'centroids'

--- mass_spectrum.py
+++ mass_spectrum.py
@@ -5,17 +5,56 @@ class mass_spectrum():
         self.mzs = []
         self.intensities = []
         self.centroids = [] 
-    def add_intensities(self,intensities):
-        self.intensities = intensities
-    def add_mzs(self,mzs):
+    ## Private basic spectrum I/O
+    def __add_mzs(self,mzs):
         self.mzs = mzs
-    def get_mzs(self):
+    def __add_intensities(self,intensities):
+        self.intensities = intensities
+    def __get_mzs(self):
 	return np.asarray(self.mzs)
-    def get_intensities(self):
+    def __get_intensities(self):
 	return np.asarray(self.intensities)
-    def add_centroids(self,mz_list,intensity_list):
+    def __get_mzs_centroids(self):
+        return np.asarray(self.centroids)
+    def __get_intensities_centroids(self):
+        return np.asarray(self.centroids_intensity)
+    def __add_centroids_mzs(self,mz_list):
 	self.centroids = mz_list
+    def __add_centroids_intensities(self,intensity_list):
 	self.centroids_intensity = intensity_list
+ 
+    ## Public methods
+    def add_spectrum(self,mzs,intensities):
+        if len(mzs) != len(intensities):
+            raise IOError("mz/intensities vector different lengths")
+        self.__add_mzs(mzs)
+        self.__add_intensities(intensities)
+    def add_centroids(self,mz_list,intensity_list):
+        if len(mz_list) != len(intensity_list):
+            raise IOError("mz/intensities vector different lengths")
+        self.__add_centroids_mzs(mz_list)
+        self.__add_centroids_intensities(intensity_list)
+    def get_spectrum(self,source='profile'):
+        if source=='profile':
+            mzs=self.__get_mzs()
+            intensities=self.__get_intensities()
+        elif source=='centroids':    
+            mzs=self.__get_mzs_centroids()
+            intensities=self.__get_intensities_centroids()
+        else:
+            raise IOError('spectrum source should be profile or centroids')
+        max_idx = np.argmax(intensities)
+        return mzs,intensities
+ 
+class MSn_spectrum(mass_spectrum):
+    def __init__(self):
+        self.ms_transitions=[]
+        self.ms_level = 1
+    def add_transition(self,transitions):
+        #transitions is a list of ms fragmentation acceptance windows
+        self.transitions = transitions
+        self.mz_level=len(self.transitions)+1
+     
 
 
 
